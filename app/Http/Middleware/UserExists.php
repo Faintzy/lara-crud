6649c85d29f891e16a16b13b0ff01d69f3bd5fbc
Gabriel (Faintzy) <gabrielmrts@yahoo.com>
2021-02-27 00:43:15 -0300

    First

--- app/Http/Middleware/UserExists.php
+++ app/Http/Middleware/UserExists.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserExists
 {
@@ -16,6 +17,21 @@ class UserExists
      */
     public function handle(Request $request, Closure $next)
     {
+        $body = $request->json()->all();
+
+        $user = DB::table('users')
+            ->where('email', $body['email'])
+            ->orWhere('username', $body['username'])
+            ->get();
+
+        if (isset($user[0]->id))
+        {
+            return response()->json([
+                'status' => 'fail',
+                'content' => 'Username or Email already exists'
+            ]);
+        }
+
         return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::put('/user/create', [UserController::class, 'create']);
+Route::put('/user/create', [UserController::class, 'create'])
+    ->middleware('user.exists');
+
+Route::post('/user/auth', [UserController::class, 'auth']);
