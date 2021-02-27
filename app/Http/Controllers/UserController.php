f50371e1b8374f31cc01fde078251ee55f4f52ca
Gabriel (Faintzy) <gabrielmrts@yahoo.com>
2021-02-27 00:43:01 -0300

    User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Helpers\Validate;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 class UserController extends Controller
@@ -36,6 +37,44 @@ class UserController extends Controller
         return response()->json([
             'status' => 'ok',
             'content' => 'User created successful'
+        ]);
+    }
+
+    public function auth(Request $request)
+    {
+        $body = $request->json()->all();
+
+        $rules = [
+            'email' => 'required|email'
+        ];
+
+        $validator = $this->getValidationFactory()
+            ->make($body, $rules);
+
+        if ($validator->fails())
+        {
+            return response()->json([
+                'status' => 'fail',
+                'content' => $validator->getMessageBag()
+            ]);
+        }
+
+        $user = DB::table('users')
+            ->where('email', $body['email'])
+            ->where('password', md5($body['password']))
+            ->get();
+
+        if (count($user) == 0)
+        {
+            return response()->json([
+                'status' => 'fail',
+                'content' => 'Incorrect credentials'
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'ok',
+            'content' => 'authenticated successful'
         ]);
     }
 }
